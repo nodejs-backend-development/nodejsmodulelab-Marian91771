e0b9aaa79da2e9561ab4867f1fdc8c072eb5587b
Vybyranyi <mariankvit@gmail.com>
2025-03-22 12:56:28 +0200

    lr2

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const http = require('http');
+const fs = require('fs');
+const zlib = require('zlib');
 
 const host = 'localhost';
 const port = 8000;
 
-const server = http.createServer((req, res) => {
-    // Replace this code by your own
-    res.writeHead(200);
-    res.end('Hello from Server!!!!');
-});
 
-server.listen(port, host, () => {
+// const server1 = http.createServer((req, res) => {
+
+//     res.writeHead(200);
+//     let name = 'marian';
+//     if(name){
+//         res.end(`Hello ${name}`);
+//     } else {
+//         res.end('You should provide name parameter');
+//     }
+
+// });
+
+// server1.listen(port, host, () => {
+//     console.log(`Server is running on http://${host}:${port}`);
+// });
+
+
+const server2 = http.createServer((req, res) => {
+    res.writeHead(200);
+    req.pipe(zlib.createGunzip())
+        .pipe(fs.createWriteStream('receivedText.txt'))
+        .on('finish', () => {
+            res.end('File received and saved');
+            console.log('Файл збережено');
+        })
+})
+
+server2.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
-});
+})
